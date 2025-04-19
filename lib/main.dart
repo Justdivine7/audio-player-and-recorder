7e88f47f26e4287e21f04b5cd906c1091317b55f
Just Divine <divineajayi179@gmail.com>
2025-04-19 15:06:21 +0100

    further separation of concerns

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:audio_player/home_page.dart';
+import 'package:audio_player/screen/home_page.dart';
 import 'package:audio_player/theme/theme_data.dart';
 import 'package:flutter/material.dart';
 
